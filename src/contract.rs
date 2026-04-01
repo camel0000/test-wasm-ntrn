dba29bd3561837699caf583f14e4a4a8a0ba73f6
camel0000 <camel0000@naver.com>
2026-04-01 16:32:54 +0900

    initial commit

---
+++ src/contract.rs
@@ -0,0 +1,27 @@
+use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw20_base::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, cw20_base::ContractError> {
+    cw20_base::contract::instantiate(deps, env, info, msg)
+}
+
+#[entry_point]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, cw20_base::ContractError> {
+    cw20_base::contract::execute(deps, env, info, msg)
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    cw20_base::contract::query(deps, env, msg)
+}
